27a3be40bb34a97b22063c2b6ba001535b00de1f
Ian Beringer <ianberinger@gmail.com>
2013-07-12 00:18:04 +0000

    error fixing....

--- web.js
+++ web.js
@@ -1,19 +1,13 @@
 var express = require('express');
-var fs = require('fs');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- fs.readFile('./index.html', function (err, index){
-     if(err){
-	 throw err;
-	 }
-     };
- response.write(index);
- response.end();
-});
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc-8', fs.readFileSync("index.html")))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
